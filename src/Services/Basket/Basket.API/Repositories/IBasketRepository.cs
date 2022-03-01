77cd3c7dea659c33d7f03de18e4fd1c1a14b2dce
Gökhan ÖZTOPUZ <gokhanoztopuz@gmail.com>
2022-03-01 10:19:24 +0300

    Basket Service Developer

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdatetBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+
+    }
+}
